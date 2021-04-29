09752922641d7136b7a029046f0ae28bc3ab5605
Cyrille Giquello <cyrille@giquello.fr>
2021-04-29 18:53:42 +0200

    XApi example

--- examples-console/example-xapi-query.php
+++ examples-console/example-xapi-query.php
@@ -9,6 +9,7 @@ use Cyrille37\OSM\Yapafo\OSM_XApi ;
 
 $query = '*[place=*][name=Artins]' ;
 //$query = 'node[amenity=hospital][bbox=-6,50,2,61]';
+//$query = '*[landuse=residential][residential=halting_site]' ;
 $xapi = new OSM_XApi();
 $res = $xapi->request( $query );
 
